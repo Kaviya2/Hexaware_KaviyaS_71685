787f930c4dc1d0f0a167a208604d0db160747f0f
Kaviya S <74661107+Kaviya2@users.noreply.github.com>
2022-05-09 08:45:20 +0530

    Add files via upload

---
+++ Python Programs/Programs_Python/Demo1.py
@@ -0,0 +1,10 @@
+#palindrome
+
+str=input("enter string:")
+print("reversed: ",end='')
+rev=str[::-1]
+print(rev)
+if rev==str:
+    print("is palindrome")
+else:
+    print("Not palindrome")
---
+++ Python Programs/Programs_Python/Demo3.py
@@ -0,0 +1,15 @@
+#Vowels
+
+str=input().lower()
+vowels=[]
+consonant=[]
+
+for i in str:
+    if(i in "aeiou"):
+        vowels.append(i)
+    elif i!=' ':
+        consonant.append(i)
+print("vowels: ",sep=" ")
+print(*vowels,sep=",")
+print("consonant: ",sep=" ")
+print(*consonant,sep=",")
